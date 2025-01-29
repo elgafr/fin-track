9716d357b4ad822c182c390b43f2b288c2632cf6
elgafr <elgaputrifr@gmail.com>
2025-01-29 12:19:07 +0700

    feat: add Navbar and ThemeSwitcherButton components

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,13 @@
-import { ReactNode } from 'react'
+import Navbar from "@/components/Navbar";
+import { ReactNode } from "react";
 
-const layout = ({children}: {children: ReactNode}) => {
+const layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
-      layout
+    <div className="relative flex h-screen w-full flex-col">
+      <Navbar />
+      <div className="w-full">{children}</div>
     </div>
-  )
-}
+  );
+};
 
-export default layout
+export default layout;
