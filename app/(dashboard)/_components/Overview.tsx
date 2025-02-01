a56a4d9e3d0ee767d53573628deba90d2d712352
elgafr <elgaputrifr@gmail.com>
2025-02-01 19:04:28 +0700

    feat(dashboard): add CategoriesStats component and update Overview & StatsCards
    - feat(api): add categories stats route
    - feat(ui): update Progress component

--- app/(dashboard)/_components/Overview.tsx
+++ app/(dashboard)/_components/Overview.tsx
@@ -8,6 +8,7 @@ import { differenceInDays, startOfMonth } from "date-fns";
 import React, { useState } from "react";
 import { toast } from "sonner";
 import StatsCards from "./StatsCards";
+import CategoriesStats from "./CategoriesStats";
 
 const Overview = ({ userSettings }: { userSettings: UserSettings }) => {
   const [dateRange, setDateRange] = useState<{ from: Date; to: Date }>({
@@ -40,8 +41,14 @@ const Overview = ({ userSettings }: { userSettings: UserSettings }) => {
           />
         </div>
       </div>
-      <div className="container flex w-full flex-col gap-2">
+      <div className="container flex w-full flex-col gap-2 p-8">
         <StatsCards
+          userSettings={userSettings}
+          from={dateRange.from}
+          to={dateRange.to}
+        />
+
+        <CategoriesStats
           userSettings={userSettings}
           from={dateRange.from}
           to={dateRange.to}
