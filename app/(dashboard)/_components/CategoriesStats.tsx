b0e6f383c877c62555575b1d31ac9f16ce7d50b0
elgafr <elgaputrifr@gmail.com>
2025-02-03 21:17:17 +0700

    refactor(dashboard): improve category stats and transaction table layout
    - Refactored `CategoriesStats.tsx` to enhance readability and maintainability.
    - Updated `layout.tsx` for better structure in the dashboard.
    - Improved `TransactionTable.tsx` to optimize rendering performance.

--- app/(dashboard)/_components/CategoriesStats.tsx
+++ app/(dashboard)/_components/CategoriesStats.tsx
@@ -93,19 +93,19 @@ const CategoriesCard = ({
                 return (
                   <div key={item.category} className="flex flex-col gap-2">
                     <div className="flex items-center justify-between">
-                      <span className="flex items-center text-gray-400">
+                      <span className="flex items-center text-muted-foreground">
                         {item.categoryIcon} {item.category}
                         <span className="ml-2 text-xs text-muted-foreground">
                             ({percentage.toFixed(0)}%)
                         </span>
                       </span>
 
-                      <span className="text-sm text-gray-400">
+                      <span className="text-sm">
                         {formatter.format(amount)}
                       </span>
                     </div>
 
-                    <Progress value={percentage} indicator={type==="income" ? "bg-emerald-500/50" : "bg-red-500/50"} />
+                    <Progress value={percentage} indicator={type==="income" ? "bg-emerald-500" : "bg-red-500"} />
                   </div>
                 );
               })}
